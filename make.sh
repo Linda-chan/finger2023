41d31556195636d794c11719c3dad0d1302c1e13
Linda Kaioh <lindaoneesama@gmail.com>
2023-07-04 02:31:03 +0300

    More warnings

--- make.sh
+++ make.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-fbc -w pedantic -exx -x test/finger2023 @finger2023.lst
+WARN_MODE=
+WARN_MODE="$WARN_MODE -w all"
+#WARN_MODE="$WARN_MODE -w none"
+WARN_MODE="$WARN_MODE -w param"
+WARN_MODE="$WARN_MODE -w Escape"
+WARN_MODE="$WARN_MODE -w pedantic"
+WARN_MODE="$WARN_MODE -w Next"
+#WARN_MODE="$WARN_MODE -w funcptr"
+#WARN_MODE="$WARN_MODE -w constness"
+WARN_MODE="$WARN_MODE -w suffix"
+#WARN_MODE="$WARN_MODE -w Error"
+WARN_MODE="$WARN_MODE -w upcast"
+
+fbc $WARN_MODE -exx -x test/finger2023 @finger2023.lst
 if [[ "$?" == "0" ]] ; then
 	if [[ "$1" == "install" ]] ; then
 		sudo cp test/finger2023 /usr/bin/finger2023
